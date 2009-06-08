6914c8c43a44b106d93c599987d6b2923ad6aafb
Nelson Loyola <loyola@cs.ualberta.ca>
2009-06-08 14:36:50 -0600

    plate INI config

---
+++ src/ScanLib.h
@@ -0,0 +1,36 @@
+#ifndef __INC_SCANLIB_H
+#define __INC_SCANLIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct sScPixelLoc {
+	int x;
+	int y;
+} ScPixelLoc;
+
+typedef struct sScPixelFrame {
+	int x0; // top
+	int y0; // left
+	int x1; // bottom
+	int y1; // right
+} ScPixelFrame;
+
+/**
+ * Specifies the region to scan.
+ */
+typedef struct sScFrame {
+	int frameId;
+	double x0; // top
+	double y0; // left
+	double x1; // bottom
+	double y1; // right
+
+} ScFrame;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INC_SCANLIB_H */
