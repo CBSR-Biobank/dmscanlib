63b8b91b21f26b2580b55453010b28f02e374f5e
Nelson Loyola <nloyola@gmail.com>
2009-06-08 23:12:09 -0600

    converting to DLL

--- src/ScanLib.cpp
+++ src/ScanLib.cpp
@@ -13,184 +13,26 @@
 #include "Dib.h"
 #include "Util.h"
 
+#ifdef WIN32
+#include "ImageGrabber.h"
+#endif
+
 #define SI_SUPPORT_IOSTREAMS
 #include "SimpleIni.h"
-
-#ifdef WIN32
-#include "getopt.h"
-#include "ImageGrabber.h"
-#else
-#include <getopt.h>
-#endif
 
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-const char * USAGE_FMT =
-	"Usage: %s [OPTIONS]\n"
-	"Test tool for scanlib library."
-	"\n"
-	"  -a, --acquire NUM     Scans an image from the scanner, corresponding to"
-	"                        the defined coordinates for plate NUM, and decodes "
-	"                        the 2D barcodes on the palette placed there.\n"
-	"  -c, --calibrate FILE  Calibrates decode regions to those found in bitmap FILE.\n"
-	"  -d, --decode FILE     Decodes the 2D barcode in the specified DIB image file.\n"
-	"  -s, --scan FILE       Scans an image and saves it as a DIB.\n"
-	"  --select              User will be asked to select the scanner.\n"
-	"  -v, --verbose NUM     Sets debugging level. Debugging messages are output "
-	"                        to stdout. Only when built UA_HAVE_DEBUG on.\n";
+const char * INI_FILE_NAME = "scanlib.ini";
 
-/* Allowed command line arguments.  */
-static struct option long_options[] = {
-		{ "acquire",   no_argument,       NULL, 'a' },
-		{ "calibrate", required_argument, NULL, 'c' },
-		{ "decode",    required_argument, NULL, 'd' },
-		{ "scan",      required_argument, NULL, 's' },
-		{ "select",    no_argument,       NULL, 200 },
-		{ "verbose",   required_argument, NULL, 'v' },
-		{ 0, 0, 0, 0 }
-};
+CSimpleIniA ini(true, false, true);
 
-#ifdef WIN32
-#define DIR_SEP_CHR '\\'
-#else
-#define DIR_SEP_CHR '/'
-#endif
-
-struct Options {
-	bool aquireAndProcessImage;
-	bool calibrate;
-	bool decodeImage;
-	bool scanImage;
-	char * filename;
-	int plateNum;
-
-	Options() {
-		aquireAndProcessImage = false;
-		calibrate = false;
-		decodeImage = false;
-		scanImage = false;
-		filename = NULL;
-	}
-};
-
-class Application {
-public:
-	Application(int argc, char ** argv);
-	~Application();
-
-private:
-	void usage();
-	void acquireAndProcesImage(unsigned plateNum);
-	void calibrateToImage(char * filename);
-	void decodeImage(char * filename);
-	void scanImage(char * filename);
-
-	static const char * INI_FILE_NAME;
-
-	const char * progname;
-	CSimpleIniA ini;
-};
-
-const char * Application::INI_FILE_NAME = "scanlib.ini";
-
-Application::Application(int argc, char ** argv) :
-	ini(true, false, true) {
-	Options options;
-	int ch;
-
-	progname = strrchr(argv[0], DIR_SEP_CHR) + 1;
-
-	UA_DEBUG(ua::DebugSinkStdout::Instance().showHeader(true);
-	ua::debugstream.sink(ua::DebugSinkStdout::Instance()));
-
-	while (1) {
-		int option_index = 0;
-
-		ch = getopt_long (argc, argv, "a:c:d:hp:s:tv:", long_options, &option_index);
-
-		if (ch == -1) break;
-		switch (ch) {
-		case 'a':
-			options.aquireAndProcessImage = true;
-			if (!Util::strToNum(optarg, static_cast<int &>(options.plateNum))) {
-				cerr << "invalid value for plate number: " << optarg << endl;
-				exit(1);
-			}
-			break;
-
-		case 'c':
-			options.calibrate = true;
-			options.filename = optarg;
-			break;
-
-		case 'd':
-			options.decodeImage = true;
-			options.filename = optarg;
-			break;
-
-		case 's':
-			options.scanImage = true;
-			options.filename = optarg;
-			break;
-
-		case 't': {
-			Dib * dib = new Dib(100, 100, 24);
-			UA_ASSERT_NOT_NULL(dib);
-			RgbQuad quad(0, 255, 0);
-			dib->line(10, 0, 80, 99, quad);
-			dib->writeToFile("out.bmp");
-			delete dib;
-			break;
-		}
-
-		case 200: {
-#ifdef WIN32
-			string err;
-			if (!ImageGrabber::Instance().selectSourceAsDefault(err)) {
-				cerr <<  err << endl;
-				exit(0);
-			}
-#else
-			cerr << "this option not allowed on your operating system." << endl;
-#endif
-			break;
-		}
-
-		case 'v': {
-			int level;
-			if (!Util::strToNum(optarg, level)) {
-				cerr << "not a valid number for verbose option: " << optarg << endl;
-				exit(1);
-			}
-			UA_DEBUG(ua::Debug::Instance().levelSet(
-					ua::DebugImpl::allSubSys_m, level));
-			break;
-		}
-
-		case '?':
-		case 'h':
-			usage();
-			exit(0);
-			break;
-
-		default:
-			cout <<  "?? getopt returned character code " << ch << " ??" << endl;
-		}
-	}
-
-	if (optind < argc) {
-		// too many command line arguments, print usage message
-		usage();
-		exit(-1);
-	}
-
-
-	/*
-	 * Loads the file if it is present.
-	 */
+/*
+ * Loads the INI file if it is present.
+ */
+void initialize() {
 	fstream inifile;
 	inifile.open(INI_FILE_NAME, fstream::in);
 	if (inifile.is_open()) {
@@ -198,36 +40,9 @@ Application::Application(int argc, char ** argv) :
 		UA_ASSERTS(rc >= 0, "attempt to load ini file returned: " << rc);
 		inifile.close();
 	}
-
-	if (options.calibrate) {
-		calibrateToImage(options.filename);
-		return;
-	}
-	else if (options.decodeImage) {
-		decodeImage(options.filename);
-		return;
-	}
-	else if (options.aquireAndProcessImage && options.scanImage) {
-		cerr << "Error: invalid options selected." << endl;
-		exit(0);
-	}
-	else if (options.aquireAndProcessImage) {
-		acquireAndProcesImage(options.plateNum);
-	}
-	else if (options.scanImage) {
-		scanImage(options.filename);
-	}
 }
 
-Application::~Application() {
-
-}
-
-void Application::usage() {
-	printf(USAGE_FMT, progname);
-}
-
-void Application::calibrateToImage(char * filename) {
+void calibrateToImage(char * filename) {
 	UA_ASSERT_NOT_NULL(filename);
 
 	Dib dib;
@@ -244,7 +59,7 @@ void Application::calibrateToImage(char * filename) {
 	markedDib.writeToFile("out.bmp");
 }
 
-void Application::decodeImage(char * filename) {
+void decodeImage(char * filename) {
 	UA_ASSERT_NOT_NULL(filename);
 
 	Dib dib;
@@ -259,7 +74,7 @@ void Application::decodeImage(char * filename) {
 	markedDib.writeToFile("out.bmp");
 }
 
-void Application::acquireAndProcesImage(unsigned plateNum) {
+void acquireAndProcesImage(unsigned plateNum) {
 #ifdef WIN32
 	string err;
 
@@ -282,7 +97,7 @@ void Application::acquireAndProcesImage(unsigned plateNum) {
 #endif
 }
 
-void Application::scanImage(char * filename) {
+void scanImage(char * filename) {
 #ifdef WIN32
 	string err;
 
@@ -300,8 +115,3 @@ void Application::scanImage(char * filename) {
 	cerr << "this option not allowed on your operating system." << endl;
 #endif
 }
-
-int main(int argc, char ** argv) {
-	Application app(argc, argv);
-}
-
