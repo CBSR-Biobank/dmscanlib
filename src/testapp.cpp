9aa2c0b96bf2c008a3666f3f34a220ed9d47b709
Thomas Polasek <thomas.p@shaw.ca>
2010-06-17 16:38:07 -0600

    implementing opencv blob finder

--- src/testapp.cpp
+++ src/testapp.cpp
@@ -25,6 +25,157 @@
 #include <bitset>
 
 
+#include <afxwin.h>
+#include "cv.h"
+#include "highgui.h"
+
+// Main blob library include
+#include "cvblob/include/BlobResult.h"
+
+char wndname[] = "Blob Extraction";
+char tbarname1[] = "Threshold";
+char tbarname2[] = "Blob Size";
+
+// The output and temporary images
+IplImage* originalThr = 0;
+IplImage* original = 0;
+IplImage* filtered = 0;
+IplImage* displayedImage = 0;
+
+int param1,param2;
+
+
+
+// threshold trackbar callback
+void on_trackbar( int dummy )
+{
+	if(!originalThr)
+	{
+		originalThr = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U,1);
+	}
+
+	if(!displayedImage)
+	{
+		displayedImage = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U,3);
+	}
+	
+	// threshold input image
+	cvThreshold( filtered, originalThr, param1, 255, CV_THRESH_BINARY );
+
+	// get blobs and filter them using its area
+	CBlobResult blobs;
+	int i;
+	CBlob *currentBlob;
+
+	// find blobs in image
+	blobs = CBlobResult( originalThr, NULL, 0 );
+	blobs.Filter( blobs, B_EXCLUDE, CBlobGetArea(), B_LESS, param2 );
+
+	// display filtered blobs
+	cvMerge( original, original, original, NULL, displayedImage );
+
+	for (i = 0; i < blobs.GetNumBlobs(); i++ )
+	{
+
+		currentBlob = blobs.GetBlob(i);
+
+		//currentBlob->FillBlob( displayedImage, CV_RGB(255,0,0));
+		
+		//cvCircle(displayedImage, cvPoint((int)currentBlob->GetEllipse().center.x,(int)currentBlob->GetEllipse().center.y), (int)(currentBlob->GetEllipse().size.width+currentBlob->GetEllipse().size.height)/2,CV_RGB(0,255,255), 2, 8, 0);
+
+		int border = 0;
+
+		CvPoint pt1, pt2;
+		pt1.x = currentBlob->GetBoundingBox().x -border;
+		pt1.y = currentBlob->GetBoundingBox().y -border;
+		pt2.x = currentBlob->GetBoundingBox().x + currentBlob->GetBoundingBox().width + border;
+		pt2.y = currentBlob->GetBoundingBox().y + currentBlob->GetBoundingBox().height + border;
+
+		cvRectangle(
+				displayedImage,
+				pt1,pt2,
+				CV_RGB(0,255,0),3, 8,0);
+
+
+	}
+    cvShowImage( wndname, displayedImage );
+}
+
+
+void applyPlateFilters(IplImage * img){
+
+	int width     = img->width;
+	int height    = img->height;
+	int depth     = img->depth;
+	int nchannels = img->nChannels;
+
+	// memory leaking?
+	IplImage* tmp = cvCreateImage(cvSize( width, height ),depth, nchannels );
+
+	for(int i=0; i < 4; i++){
+		cvSmooth(img,tmp,CV_GAUSSIAN,11,11);
+		cvSmooth(tmp,img,CV_GAUSSIAN,11,11);
+	}
+	cvSmooth(img,tmp,CV_GAUSSIAN,11,11);
+}
+
+
+
+int main( int argc, char** argv )
+{
+	if(argc < 2){
+		exit(-1);
+	}
+
+	param1 = 50;
+	param2 = 2000;
+	
+	original  = cvLoadImage(argv[1],0);
+	filtered  = cvLoadImage(argv[1],0);
+
+	cvNamedWindow("input",0);
+	cvResizeWindow("input",800,600);
+	cvShowImage("input", original );
+
+	applyPlateFilters(filtered);
+
+	cvNamedWindow("filtered",0);
+	cvResizeWindow("filtered",800,600);
+	cvShowImage("filtered", filtered );
+
+	cvNamedWindow(wndname, 0);
+	cvResizeWindow(wndname,800,600);
+    cvCreateTrackbar( tbarname1, wndname, &param1, 255, on_trackbar );
+	cvCreateTrackbar( tbarname2, wndname, &param2, 30000, on_trackbar );
+
+
+	
+	// Call to update the view
+	for(;;)
+    {
+        int c;
+        
+        // Call to update the view
+        on_trackbar(0);
+
+        c = cvWaitKey(0);
+
+	   if( c == 27 )
+            break;
+	}
+    
+    cvReleaseImage( &original );
+	cvReleaseImage( &originalThr );
+	cvReleaseImage( &displayedImage );
+    
+    cvDestroyWindow( wndname );
+    
+    return 0;
+}
+
+
+
+
 #ifndef WIN32
 #   include <limits>
 #   include <bitset>
@@ -689,8 +840,12 @@ bool Application::getCmdOptions(int argc, char ** argv) {
    }
    return true;
 }
-
+/*
 int main(int argc, char ** argv) {
    Application app(argc, argv);
 }
+
+// main.cpp : Defines the entry point for the console application.
+//
+*/
 
