2d054ee20c3deb2a8dab51e802c1d531f9f037b5
Nelson Loyola <loyola@cs.ualberta.ca>
2010-09-14 18:15:51 -0600

    moved profile settings into PalletGrid

---
+++ src/PalletGrid.cpp
@@ -0,0 +1,95 @@
+/*
+ * CellPosition.cpp
+ *
+ * Dmscanlib is a software library and standalone application that scans
+ * and decodes Libdmtx compatible test-tubes. It is currently designed
+ * to decode 12x8 pallets that use 2D data-matrix laser etched test-tubes.
+ * Copyright (C) 2010 Canadian Biosample Repository
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PalletGrid.h"
+#include "UaAssert.h"
+#include "cxtypes.h"
+
+#include <sstream>
+
+PalletGrid::PalletGrid(Orientation o, unsigned imgWidth, unsigned imgHeight,
+		unsigned gapX, unsigned gapY, vector<unsigned> & profileWords) {
+	orientation = o;
+
+	if (orientation == ORIENTATION_HORIZONTAL) {
+		cellWidth = imgWidth / MAX_COLS;
+		cellHeight = imgHeight / MAX_ROWS;
+	} else if (orientation == ORIENTATION_VERTICAL) {
+		cellWidth = imgWidth / MAX_ROWS;
+		cellHeight = imgHeight / MAX_COLS;
+	} else {
+		UA_ASSERTS(false, "orientation invalid: " << orientation);
+	}
+
+	this->gapX = gapX;
+	this->gapY = gapY;
+
+	// load the profile
+	UA_ASSERTS(profileWords.size() == 3, "invalid size for profileWords");
+	unsigned mask;
+	for (unsigned i = 0; i < NUM_WORDS; ++i) {
+		mask = 1;
+		for (unsigned j = 0; j < 32; ++j) {
+			if (profileWords[i] & mask) {
+				bits[32 * i + j] = 1;
+			}
+			mask <<= 1;
+		}
+	}
+}
+
+PalletGrid::~PalletGrid() {
+}
+
+void PalletGrid::getImageCoordinates(unsigned row, unsigned col, CvRect & rect) {
+	UA_ASSERT(row < MAX_ROWS);
+	UA_ASSERT(col < MAX_COLS);
+
+	if (orientation == ORIENTATION_HORIZONTAL) {
+		rect.x = cellWidth * (MAX_COLS - col - 1) + gapX;
+		rect.y = cellHeight * (MAX_ROWS - row - 1) + gapY;
+	} else if (orientation == ORIENTATION_VERTICAL) {
+		rect.x = cellHeight * (MAX_COLS - col - 1) + gapY;
+		rect.y = cellWidth * (MAX_ROWS - row - 1) + gapX;
+	} else {
+		UA_ASSERTS(false, "orientation invalid: " << orientation);
+	}
+
+	rect.width = cellWidth - 2 * gapX;
+	rect.height = cellWidth - 2 * gapY;
+}
+
+void PalletGrid::getPositionStr(unsigned row, unsigned col, string & str) {
+	UA_ASSERT(row < MAX_ROWS);
+	UA_ASSERT(col < MAX_COLS);
+
+	ostringstream out;
+	out << (char) ('A' + row) << col;
+	str = out.str();
+}
+
+
+bool PalletGrid::getCellEnabled(unsigned row, unsigned col) {
+	UA_ASSERTS(row < PalletGrid::MAX_ROWS, "invalid row requested " << row);
+	UA_ASSERTS(col < PalletGrid::MAX_COLS, "invalid column requested " << col);
+	return bits[PalletGrid::MAX_COLS * row + col];
+}
---
+++ src/PalletGrid.h
@@ -0,0 +1,91 @@
+#ifndef PALLET_GRID_H_
+#define PALLET_GRID_H_
+
+/*
+ * CellPosition.h
+ *
+ * Dmscanlib is a software library and standalone application that scans
+ * and decodes libdmtx compatible test-tubes. It is currently designed
+ * to decode 12x8 pallets that use 2D data-matrix laser etched test-tubes.
+ * Copyright (C) 2010 Canadian Biosample Repository
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <vector>
+
+struct CvRect;
+
+using namespace std;
+
+class PalletGrid {
+public:
+	enum Orientation {
+		ORIENTATION_HORIZONTAL, ORIENTATION_VERTICAL
+	};
+
+	static const unsigned MAX_ROWS = 8;
+
+	static const unsigned MAX_COLS = 12;
+
+	static const unsigned NUM_CELLS = MAX_ROWS * MAX_COLS;
+
+	/**
+	 * Constructs a pallet grid with cells cellWidth pixels and
+	 * cellHeight pixels.
+	 *
+	 * @param o The orientation of the grid. Either ORIENTATION_HORIZONTAL
+	 * or ORIENTATION_VERTICAL.
+	 *
+	 * @param imgWidth The width of the image in pixels.
+	 *
+	 * @param imgHeight The height of the image in pixels.
+	 *
+	 * @param gapX The horizontal gap between cells in pixels.
+	 *
+	 * @param gapY The vertical gap between cells in pixels.
+	 */
+	PalletGrid(Orientation o, unsigned imgWidth, unsigned imgHeight,
+			unsigned gapX, unsigned gapY, vector<unsigned> & profileWords);
+
+	~PalletGrid();
+
+	void getImageCoordinates(unsigned row, unsigned col, CvRect & rect);
+
+	void getPositionStr(unsigned row, unsigned col, string & str);
+
+	/**
+	 * Returns wether or not the pallet location should be decoded.
+	 *
+	 * @param row a number between 0 and PalletGrid::MAX_ROWS - 1.
+	 * @param col a number between 0 and PalletGrid::MAX_COLS - 1.
+	 *
+	 * @return true if the cell should be decoded.
+	 */
+	bool getCellEnabled(unsigned row, unsigned col);
+
+private:
+	static const unsigned NUM_WORDS = PalletGrid::NUM_CELLS / 32;
+
+	unsigned orientation;
+	unsigned cellWidth;
+	unsigned cellHeight;
+	unsigned gapX;
+	unsigned gapY;
+	vector<bool> bits;
+
+};
+
+#endif /* PALLET_GRID_H_ */
