2d8f25bdcd283fc82975a64f25fd77aa08b0c588
Thomas Polasek <tpolasek@ualberta.ca>
2011-08-02 10:38:56 -0600

    updated image renaming script and added renamed test images

--- tests/images/rename.py
+++ tests/images/rename.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 import os
 import os.path
+import sys
 import hashlib
+
+
+path = '.'
+listing = os.listdir(path)
 
 def md5Checksum(filePath):
     fh = open(filePath, 'rb')
@@ -13,10 +18,22 @@ def md5Checksum(filePath):
         m.update(data)
     return m.hexdigest()
 
-path = '.'
-listing = os.listdir(path)
+def main(argv):
+	
+	if len(argv) != 1 or argv[0] != "rename":
+		print "Renames all the bmp files by there md5 hash in this scripts working directory."
+		print "Usuage thiscript.py rename."
+		return
 
-for infile in listing:
-	basename, ext = os.path.splitext(infile)
-	if ext == ".bmp":
-		os.rename(infile,md5Checksum(infile) + ".bmp")
+	print "Renaming files...\n"
+	for infile in listing:
+		basename, ext = os.path.splitext(infile)
+		if ext == ".bmp":
+			md5check = md5Checksum(infile) +".bmp"
+			print "renaming %s to %s" % (infile,md5check)
+			os.rename(infile, md5check)
+	
+	print "Renamed files.\n"
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
